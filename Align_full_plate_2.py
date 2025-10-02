8e50d6db6e80a6ad51d1ae6d8ef1807e5acf0be0
mikepassal <mikepassalacqua@gmail.com>
2025-10-02 16:53:16 -0400

    Next_3_plate_align

---
+++ Align_full_plate_2.py
@@ -0,0 +1,23 @@
+import time
+import os
+
+# 
+start_time = time.time()
+
+data_directory = "/scratch/cgsb/bergelson/bergelson-lab/LH_LIC2024/rnaseq/licrna2"
+genome_directory = "~/Genomes/arabidopsis_thaliana"
+output_directory = "/scratch/mp7563/Luke_terrace_10_job_sample/aligned_data/plate_2"
+list_of_samples_to_process = os.listdir(data_directory)
+number_of_cores_to_use = 48
+os.system(f"STAR --genomeLoad LoadAndExit --genomeDir {genome_directory}")
+
+for sample in list_of_samples_to_process:
+    os.system(f"STAR --runThreadN {number_of_cores_to_use} --genomeDir {genome_directory} --readFilesIn {data_directory}/{sample} --outFileNamePrefix {output_directory}/{sample}_  --quantMode GeneCounts --outSAMtype None")
+os.system(f"STAR --genomeLoad Remove --genomeDir {genome_directory}")
+end_time = time.time()
+change_in_time = end_time - start_time
+change_in_time_minutes = change_in_time / 60
+timer_file_name = "/home/mp7563/Jobs_logs/Luke_terrace_jobs/Testing_aligning_10_samples/Testing_aligning_full_plate_number_2_samples_using_48_cores.txt"
+with open(timer_file_name, "w") as text_file:
+    
+    text_file.write(f"Time taken to align full plate of samples using 48 cores: {change_in_time_minutes} minutes")
---
+++ Align_full_plate_4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=05:00:00
+#SBATCH --mem=40GB
+#SBATCH --job-name=Align_plate_4
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michael.p@nyu.edu
+#SBATCH --output=/home/mp7563/Jobs_logs/Luke_terrace_jobs/Testing_aligning_full_plate_samples/full_plate_4_samples_proper_format.out
+#SBATCH --error=/home/mp7563/Jobs_logs/Luke_terrace_jobs/Testing_aligning_full_plate_samples/full_plate_4_samples_proper_format.err
+
+module purge 
+module load star/intel/2.7.11a
+
+
+singularity exec --overlay /home/mp7563/Python_envs/Python_envs.ext3:ro /home/mp7563/Python_envs/ubuntu_cuda_image.sif /bin/bash -c "source /ext3/env.sh; conda activate Luke_terrace; module load star/intel/2.7.11a; python /home/mp7563/Git/Outdoor_microbiome/Align_full_plate_4.py"
+
+
+
