69fdcb12999eaf87358462cf115b27182c5c6fd8
mikepassal <mikepassalacqua@gmail.com>
2025-10-10 16:00:37 -0400

    update py and shell

--- Cluster_analysis/test_sra_lite.py
+++ Cluster_analysis/test_sra_lite.py
@@ -1,8 +1,9 @@
 import os
-list_of_folders_to_run_STAR_in = ['/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/ERR10962770',
-                                  '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR30593421',
-                                  '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR31112633',
-                                  '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR34697341',
+list_of_folders_to_run_STAR_in = [
+  #                                '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/ERR10962770',
+  #                                '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR30593421',
+   #                               '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR31112633',
+   #                               '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_lite/SRR34697341',
                                   '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_reg/ERR10962770',
                                   '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_reg/SRR30593421',
                                   '/scratch/mp7563/Luke_terrace_10_job_sample/SRA_lite_vs_reg/sra_reg/SRR31112633',
@@ -20,9 +21,9 @@ for file in list_of_folders_to_run_STAR_in:
     if num_files ==2:
             matched_file = [s for s in current_files if '.fastq' in s]
             print(f'Running {file}')
-            os.system(f'STAR --runThreadN 11 --genomeDir {genome_dir} --readFilesIn {file}/{matched_file[0]}  --outFileNamePrefix {file}/{accession} --outSAMtype None --quantMode GeneCounts')
+            os.system(f'STAR --runThreadN 24 --genomeDir {genome_dir} --readFilesIn {file}/{matched_file[0]}  --outFileNamePrefix {file}/{accession} --outSAMtype None --quantMode GeneCounts')
     elif num_files == 3:
                 matched_read_1 = [s for s in current_files if '_1.fastq' in s]
                 matched_read_2 = [s for s in current_files if '_2.fastq' in s]  
-                os.system(f'STAR --runThreadN 11 --genomeDir {genome_dir} --readFilesIn {file}/{matched_read_1[0]} {file}/{matched_read_2[0]}  --outFileNamePrefix {file}/{accession} --outSAMtype None --quantMode GeneCounts')
+                os.system(f'STAR --runThreadN 24 --genomeDir {genome_dir} --readFilesIn {file}/{matched_read_1[0]} {file}/{matched_read_2[0]}  --outFileNamePrefix {file}/{accession} --outSAMtype None --quantMode GeneCounts')
 os.system(f"STAR --genomeLoad Remove --genomeDir {genome_dir}")
