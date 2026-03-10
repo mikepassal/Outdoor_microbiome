d67b245251491c464a5b09c7726218067d9dc5f3
mikepassal <mikepassalacqua@gmail.com>
2026-03-10 14:49:35 -0400

    fix path typo

--- Cluster_analysis/Align_full_plate_5.sh
+++ Cluster_analysis/Align_full_plate_5.sh
@@ -5,9 +5,7 @@
 #SBATCH --time=04:00:00
 #SBATCH --mem=40GB
 #SBATCH --job-name=Align_plate_5
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=END
-#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END,FAIL,BEGIN
 #SBATCH --account=torch_pr_121_general
 #SBATCH --mail-user=michael.p@nyu.edu
 #SBATCH --output=/home/mp7563/Jobs_logs/Luke_terrace_jobs/Testing_aligning_full_plate_samples/full_plate_5_samples_proper_format.out
@@ -19,4 +17,4 @@ stdbuf -o0 echo "Script Started"
 singularity exec --nv --overlay \
     /home/mp7563/Python_envs/Torch_miniforge_env.ext3:ro /home/mp7563/Python_envs/ubuntu_cuda_image.sif \
     /bin/bash -c "source /ext3/env.sh; conda activate Luke_terrace; \
-    python /home/mp7563/Git/Outdoor_microbiome/Align_full_plate_5.py"
+    python /home/mp7563/Git/Outdoor_microbiome/Cluster_analysis/Align_full_plate_5.py"
